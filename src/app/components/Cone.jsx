cd33ae4863db9c1ef6ef738690a9ece38ab7d633
Piotr Archi <piotr.borysowski@gmail.com>
2024-05-11 09:41:43 +0200

    add extra function for testing different shape

--- src/app/components/Cone.jsx
+++ src/app/components/Cone.jsx
@@ -5,14 +5,32 @@ const fragmentShader = `
 uniform float iTime;
 varying vec2 vUv;
 
-vec3 colorA = vec3(0.912,0.191,0.652);
-
 vec3 palette(float t) {
     vec3 a = vec3(1.198, 0.628, -0.092);
     vec3 b = vec3(1.420, -0.259, 0.843);
     vec3 c = vec3(0.816, 5.712, 3.874);
     vec3 d = vec3(-1.523, -0.453, 0.047);
     return a + b*cos(6.28318*(c*t+d));
+}
+
+float sdPentagon( in vec2 p, in float r )
+{
+    const vec3 k = vec3(0.809016994,0.587785252,0.726542528);
+    p.x = abs(p.x);
+    p -= 2.0*min(dot(vec2(-k.x,k.y),p),0.0)*vec2(-k.x,k.y);
+    p -= 2.0*min(dot(vec2( k.x,k.y),p),0.0)*vec2( k.x,k.y);
+    p -= vec2(clamp(p.x,-r*k.z,r*k.z),r);    
+    return length(p)*sign(p.y);
+}
+
+float sdHexagram( in vec2 p, in float r )
+{
+    const vec4 k = vec4(-0.5,0.8660254038,0.5773502692,1.7320508076);
+    p = abs(p);
+    p -= 2.0*min(dot(k.xy,p),0.0)*k.xy;
+    p -= 2.0*min(dot(k.yx,p),0.0)*k.yx;
+    p -= vec2(clamp(p.x,r*k.z,r*k.w),r);
+    return length(p)*sign(p.y);
 }
 
 void main() { 
